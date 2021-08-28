25c905e4ef1f6a97a7606785bccb0d7f2967b394
Dmitrii Chudinov <d.chudinov.spb@yandex.ru>
2021-08-28 04:22:29 +0300

    Add python parser for habr_career

---
+++ sources/habr_career/index.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import requests
+from parse import search_company 
+
+def handler(event, context):
+    # defaultLimit = 5
+    limit = 5
+    maxLimit = 25
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    # Get name by q
+    name = ''
+    if 'queryStringParameters' in event and 'q' in event['queryStringParameters']:
+        name = event['queryStringParameters']['q']
+    if name == '':
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'isBase64Encoded': False,
+            'body': {
+                "error":True,
+                "message":"set q param"
+            }
+        }
+    # Get limit
+    if 'queryStringParameters' in event and 'limit' in event['queryStringParameters']:
+        limit = event['queryStringParameters']['limit']
+    if limit > maxLimit:
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'isBase64Encoded': False,
+            'body': {
+                "error":True,
+                "message":"limit is more than max"
+            }
+        }
+    companies = search_company(name, int(limit))
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'isBase64Encoded': False,
+        'body': {
+            "companies":companies,
+        }
+    }

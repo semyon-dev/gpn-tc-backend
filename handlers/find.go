644bd0c9976444cc43094c2d0f17a5448a6ea71e
Semyon Novikov <semyon-dev@protonmail.com>
2021-08-28 04:43:16 +0300

    add new finder

--- handlers/find.go
+++ handlers/find.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/semyon-dev/gpn-tc-backend/db"
 	"github.com/semyon-dev/gpn-tc-backend/sources"
 	"log"
 	"net/http"
@@ -17,16 +18,18 @@ func Find(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "bad req"})
 		return
 	}
-	items, err := sources.ParseHH(jsonInput.Name)
+	itemsHH, err := sources.ParseHH(jsonInput.Name)
+	itemsRospatent := db.FindInUtilityModel(jsonInput.Name)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "internal server error",
-			"items":   items,
 		})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"message": "ok",
-		"items":   items,
+		"message":     "ok",
+		"hh":          itemsHH,
+		"rospatent":   itemsRospatent,
+		"habr-career": itemsRospatent,
 	})
 }
